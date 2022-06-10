476113d3e70c6eea6cbbaa5b731731fc1fb51624
Regucki <sebastian.regucki@capgemini.com>
2022-06-10 21:37:19 +0200

    Regexes to validate email and phone number

--- src/main/java/model/Validation.java
+++ src/main/java/model/Validation.java
@@ -1,9 +1,13 @@
 package model;
 
+import lombok.NoArgsConstructor;
+
 import java.util.regex.Pattern;
 
+
+@NoArgsConstructor
 public abstract class Validation {
-    public Validation() {}
+
 
     public static boolean validateTeleNumb(String teleNumb){return Pattern.compile("^[0-9]{3}-[0-9]{3}-[0-9]{3}$").matcher(teleNumb).matches();}
     public static boolean validateEmail(String email){return Pattern.compile("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$").matcher(email).matches();}
