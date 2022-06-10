c61c44bb823cc9b5229c8ef979dfde1fb02ff120
Regucki <sebastian.regucki@capgemini.com>
2022-06-10 21:56:05 +0200

    moved entities dedicated package

--- src/main/java/model/entities/PersonalAccount.java
+++ src/main/java/model/entities/PersonalAccount.java
@@ -1,11 +1,11 @@
-package model;
+package model.entities;
 
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class PersonalAccount extends Account{
-    
+
     public PersonalAccount(double balance) {
         super(balance);
     }
