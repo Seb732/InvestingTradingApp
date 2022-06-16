594ecf8d2084b569429b99a48f7b85a4038ee8a9
Sebastian Regucki <sebreg11@gmail.com>
2022-06-16 15:15:11 +0200

    Added javadocs for account class

--- src/main/java/model/entities/Account.java
+++ src/main/java/model/entities/Account.java
@@ -1,36 +1,49 @@
 package model.entities;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Abstract class for user's account
+ */
+@NoArgsConstructor
+@Getter
+@Setter
 public abstract class Account {
+    /**
+     * the account balance field
+     */
     double balance;
 
-    public Account() {
-    }
 
     public Account(double balance) {
         this();
         setBalance(balance);
     }
 
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
+    /**
+     * This method enables user to deposit money
+     * @param amount - amount of money to be deposited
+     */
     public void deposit(double amount){
         if (amount > 1){
             this.balance += amount;
         }
     }
-    public void withdraw(double amount) throws IllegalAccessException {
+
+    /**
+     * This method enables user to withdraw money from account
+     * @param amount - amount of money to be withdrawn
+     * @throws IllegalAccessException - if the amount is overdrawn, then method throws exception
+     */
+    public void withdraw(double amount) throws Exception {
         if (amount <= this.balance){
             this.balance -= amount;
         }
         else{
-            throw new IllegalAccessException();
+            throw new Exception("Overdraft!");
         }
     }
 }
