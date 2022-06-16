f7594b8c421d93b158212f7370dd2ca5f1ded0d8
Sebastian Regucki <sebreg11@gmail.com>
2022-06-16 15:40:37 +0200

    added javadoc for investing account class

--- src/main/java/model/entities/InvestingAccount.java
+++ src/main/java/model/entities/InvestingAccount.java
@@ -3,7 +3,9 @@ package model.entities;
 public class InvestingAccount extends Account{
 
 
-
+    /**
+     * Class for user's investing account. Content development in a progress
+     */
     public InvestingAccount() {
         this.balance = 0;
     }
