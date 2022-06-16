e22fb09c466e6600f1b83ebbc93c21f3a2dd843a
Sebastian Regucki <sebreg11@gmail.com>
2022-06-16 15:24:51 +0200

    Added javadoc for user class

--- src/main/java/model/entities/User.java
+++ src/main/java/model/entities/User.java
@@ -11,6 +11,12 @@ import model.exceptions.TeleNumbException;
 @Setter
 @ToString
 @EqualsAndHashCode
+
+
+/**
+ * This class represents user's entity. Stores all information fields required. Contains method which enables user
+ * to transfer money from personal to investing account and vice-versa.
+ */
 public class User {
     long id;
     String firstName;
@@ -20,27 +26,39 @@ public class User {
     PersonalAccount personalAccount;
     InvestingAccount investingAccount;
 
-
+    /**
+     * This metod enables user to transfer money to be invested using investing account.
+     * @param amount - amount of money to be transferred
+     */
     public void transferIN(double amount){
         try{
             this.getPersonalAccount().withdraw(amount);
             this.getInvestingAccount().deposit(amount);
         }
-        catch (IllegalArgumentException | IllegalAccessException ex){
+        catch (Exception ex){
             assert true; //TODO
         }
     }
 
+    /**
+     * This method enables user to withdraw money from investing account.
+     * @param amount - amount of money to transferred out of the investing account
+     */
     public void transferOut(double amount){
         try{
             this.getInvestingAccount().withdraw(amount);
             this.getPersonalAccount().deposit(amount);
         }
-        catch (IllegalAccessException | IllegalArgumentException ex) {
+        catch (Exception ex) {
             assert true; //TODO
         }
     }
 
+    /**
+     * This method validates the correctness of user's phone number.
+     * @param teleNumb - user's phone number
+     * @throws TeleNumbException - if phone number is not correct then custom exception is being thrown
+     */
     public void setTeleNumb(String teleNumb) throws TeleNumbException {
         if (!Validation.validateTeleNumb(teleNumb)){
             throw new TeleNumbException("Incorrect phone number");
@@ -48,6 +66,11 @@ public class User {
         this.teleNumb = teleNumb;
     }
 
+    /**
+     * This method validates the correctness of user's email.
+     * @param email - user's email
+     * @throws EmailException - if email is not correct then custom exception is being thrown
+     */
     public void setEmail(String email) throws EmailException {
         if (!Validation.validateEmail(email)){
             throw new EmailException("Incorrect email");
