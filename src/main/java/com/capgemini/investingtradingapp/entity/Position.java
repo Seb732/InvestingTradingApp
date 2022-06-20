fc722ca3bdec50a6c83a356e6ed586205fa3dde0
Sebastian Regucki <sebreg11@gmail.com>
2022-06-20 15:59:56 +0200

    Added JPA annotations for position class

--- src/main/java/com/capgemini/investingtradingapp/entity/Position.java
+++ src/main/java/com/capgemini/investingtradingapp/entity/Position.java
@@ -2,9 +2,12 @@ package com.capgemini.investingtradingapp.entity;
 
 
 import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
+
 @ToString
 @Getter
 @Setter
@@ -12,11 +15,29 @@ import lombok.*;
 /**
  * This class represents single stock in user's portfolio.
  */
+@Entity
+@Table(name = "position")
 public class Position {
     /**
      * ticker - current stock price, size - number of bought shares
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "positionid", nullable = false)
+    long positionID;
+
+    @Column(name = "companyid", nullable = false)
     String companyID;
+    @Column(name = "size", nullable = false)
     int size;
-    Double ticker;
+    @Column(name = "ticker", nullable = false)
+    double ticker;
+    @Column(name = "userid", nullable = false)
+    long userID;
+
+    public Position(String companyID, int size, double ticker){
+        this.setCompanyID(companyID);
+        this.setSize(size);
+        this.setTicker(ticker);
+    }
 }
