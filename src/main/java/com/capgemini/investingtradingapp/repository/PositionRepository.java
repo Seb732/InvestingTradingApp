b0a38058e3f01de53b09241e5a9c747d6bdf36f9
Sebastian Regucki <sebreg11@gmail.com>
2022-06-20 15:55:29 +0200

    Added Position class repository

---
+++ src/main/java/com/capgemini/investingtradingapp/repository/PositionRepository.java
@@ -0,0 +1,10 @@
+package com.capgemini.investingtradingapp.repository;
+
+import com.capgemini.investingtradingapp.entity.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PositionRepository extends JpaRepository<Position, Long> {
+}
