85a514e2d4c754dbcfa6d680f06655084c124a6b
Sebastian Regucki <sebreg11@gmail.com>
2022-06-20 16:54:26 +0200

    Added user class repository

---
+++ src/main/java/com/capgemini/investingtradingapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.capgemini.investingtradingapp.repository;
+
+import com.capgemini.investingtradingapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
