b8ab1318a7a10366851209602de944a5522894cd
Sebastian Regucki <sebreg11@gmail.com>
2022-06-22 11:10:46 +0200

    Added queries to position class repository

--- src/main/java/com/capgemini/investingtradingapp/repository/PositionRepository.java
+++ src/main/java/com/capgemini/investingtradingapp/repository/PositionRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
+    Optional<Position> findPositionByCompanyID(String companyID);
+    Optional<Position> findPositionByOpenDateAfter(LocalDateTime openDate);
+    Optional<Position> findPositionByTickerGreaterThan(double ticker);
 }
