f020959e8e24d9e6f313958e13e1a15efa26207a
Sebastian Regucki <sebreg11@gmail.com>
2022-06-23 16:27:18 +0200

    Added methods to position repository

--- src/main/java/com/capgemini/investingtradingapp/repository/PositionRepository.java
+++ src/main/java/com/capgemini/investingtradingapp/repository/PositionRepository.java
@@ -2,7 +2,6 @@ package com.capgemini.investingtradingapp.repository;
 
 import com.capgemini.investingtradingapp.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
