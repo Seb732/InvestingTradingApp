bb30c5395f89ab7833b4642cd86622c7bf7f5419
Sebastian Regucki <sebreg11@gmail.com>
2022-06-28 18:54:35 +0200

    Added mapping in new user creation method

--- src/main/java/com/capgemini/investingtradingapp/service/UserService.java
+++ src/main/java/com/capgemini/investingtradingapp/service/UserService.java
@@ -1,9 +1,10 @@
 package com.capgemini.investingtradingapp.service;
 
+import com.capgemini.investingtradingapp.dto.UserDTO;
 import com.capgemini.investingtradingapp.entity.User;
 import com.capgemini.investingtradingapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +13,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    @Autowired
+    private final ModelMapper modelMapper;
 
 
-    public Iterable<User> GetAll(){
+    public Iterable<User> GetAll() {
         return userRepository.findAll();
     }
 
-    public User createUser(User user){
-        return userRepository.save(user);
+    public UserDTO save(UserDTO userDTO) {
+        User user = userRepository.save(modelMapper.map(userDTO, User.class));
+        return modelMapper.map(user, UserDTO.class);
     }
 }
