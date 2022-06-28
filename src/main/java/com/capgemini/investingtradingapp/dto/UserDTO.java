52ee4c9aa4a827737d6cd76310ec6dc14652c884
Sebastian Regucki <sebreg11@gmail.com>
2022-06-28 18:46:09 +0200

    Added user's data transfer object class

---
+++ src/main/java/com/capgemini/investingtradingapp/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.capgemini.investingtradingapp.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String teleNumb;
+    private String email;
+}
