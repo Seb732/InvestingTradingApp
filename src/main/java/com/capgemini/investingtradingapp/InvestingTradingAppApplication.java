6c104bf5b065a051b9c341b1d44abc8a253c46dd
Sebastian Regucki <sebreg11@gmail.com>
2022-06-28 18:47:00 +0200

    Temporarily disabled auth in app

--- src/main/java/com/capgemini/investingtradingapp/InvestingTradingAppApplication.java
+++ src/main/java/com/capgemini/investingtradingapp/InvestingTradingAppApplication.java
@@ -1,12 +1,10 @@
 package com.capgemini.investingtradingapp;
 
-import com.capgemini.investingtradingapp.service.PositionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class InvestingTradingAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(InvestingTradingAppApplication.class, args);
