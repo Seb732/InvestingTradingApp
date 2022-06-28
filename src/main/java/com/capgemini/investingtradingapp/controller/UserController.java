e68016d6e6bedaa0794b093e028180516b8fae36
Sebastian Regucki <sebreg11@gmail.com>
2022-06-28 20:30:02 +0200

    Fixed incorrect URI in user controller

--- src/main/java/com/capgemini/investingtradingapp/controller/UserController.java
+++ src/main/java/com/capgemini/investingtradingapp/controller/UserController.java
@@ -3,19 +3,29 @@ package com.capgemini.investingtradingapp.controller;
 import com.capgemini.investingtradingapp.dto.UserDTO;
 import com.capgemini.investingtradingapp.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 
+@RequestMapping("/employee")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/employee/new")
+
+    @PostMapping
     public UserDTO newUser(@RequestBody final UserDTO user) {
         return userService.save(user);
     }
 
+    @GetMapping
+    @ResponseBody
+    public Optional<UserDTO> getUserByFirstNameAndLastName(
+            @RequestParam final String firstName,
+            @RequestParam final String lastName
+    ) {
+        return userService.findByFirstNameAndLastName(firstName, lastName);
+    }
 }
